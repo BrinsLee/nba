8e6fedd3702cc6cd555e71434e94c7c78ce8828e
BrinsLee <747127514@qq.com>
2020-07-09 18:23:36 +0800

    提交：首次提交

---
+++ app/src/main/java/com/brins/nba/api/RetrofitFactory.kt
@@ -0,0 +1,45 @@
+package com.brins.nba.api
+
+import com.brins.nba.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+class RetrofitFactory {
+
+    companion object {
+        private val TIMEOUT_CONNECTED: Long = 15
+        private val TIMEOUT_READ: Long = 15
+        private val TIMEOUT_WRITE = 60
+
+        fun newRetrofit(baseUrl: String, tag: String): Retrofit {
+            val retrofit = Retrofit.Builder().baseUrl(baseUrl)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(getClient(tag))
+                .build()
+            return retrofit
+        }
+
+        private fun getClient(logTag: String): OkHttpClient {
+            val builder: OkHttpClient.Builder = OkHttpClient().newBuilder()
+            builder.connectTimeout(TIMEOUT_CONNECTED, TimeUnit.SECONDS)
+                .readTimeout(TIMEOUT_READ, TimeUnit.SECONDS)
+            /*.addInterceptor(AddCookiesInterceptor())
+            .addInterceptor(ReceivedCookiesInterceptor())*/
+
+            if (BuildConfig.DEBUG) {
+                builder.addInterceptor(
+                    HttpLoggingInterceptor()
+                        .setLevel(HttpLoggingInterceptor.Level.BODY)
+                )
+            }
+            return builder.build()
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/brins/nba/api/ServiceApi.kt
@@ -0,0 +1,8 @@
+package com.brins.nba.api
+
+/**
+ * @author lipeilin
+ * @date 2020/7/9
+ */
+val APPKEY = "6fc18957ce391f84a7ce34ce13cd99c4"
+val DOMAIN = "https://wapapi.it919.cn/?service="
